33446a1e6f56a5fa71e933f42ead0959d9af7a3b
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2025-07-06 20:17:26 +0800

    feat: add NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import gsap from "gsap";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello World</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   );
 };
 export default App;
