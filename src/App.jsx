76bcac5529dffb38616477df0f54eae2f99da437
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2025-07-07 09:05:56 +0800

    feat: add cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 import Navbar from './components/Navbar';
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <Hero/>
-      <div className="h-dvh bg-black"></div>
+      <Cocktails/>
     </main>
   );
 };
